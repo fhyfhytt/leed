32baec4a547682e7fc16230c2c2e5a255b5bb15b
YSlty <46297334+YSlty@users.noreply.github.com>
2019-02-22 16:23:09 +0800

    Add files via upload

---
+++ leed/js/nav.js
@@ -0,0 +1,8 @@
+$(".nav li").click(function(){
+	$(this).addClass("col").siblings().removeClass("col")
+	// console.log($(this).text())
+})
+$(".nav2 li").click(function(){
+	$(this).addClass("colo").siblings().removeClass("colo")
+	// console.log($(this).text())
+})
